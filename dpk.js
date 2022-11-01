028f2d136641e6d97e832224342f816e76cb6dbc
Pavithra Devi <65974841+prajkuma@users.noreply.github.com>
2022-10-31 23:12:44 -0700

    Update dpk.js

--- dpk.js
+++ dpk.js
@@ -4,25 +4,14 @@ exports.deterministicPartitionKey = (event) => {
   const TRIVIAL_PARTITION_KEY = "0";
   const MAX_PARTITION_KEY_LENGTH = 256;
   let candidate = TRIVIAL_PARTITION_KEY;
-  let checkhash=false;
-
+ 
   if (event) {
     if (event.partitionKey) {
-      candidate = event.partitionKey;
+      candidate = crypto.createHash("sha3-512").update(JSON.stringify(event.partitionKey)).digest("hex");
     } else {
       candidate = crypto.createHash("sha3-512").update(JSON.stringify(event)).digest("hex");
-      checkhash=true;
+      
     }
   }
-
-  if (candidate) {
-    if (typeof candidate !== "string" ) {
-      candidate = JSON.stringify(candidate);
-    }
-    if (candidate.length > MAX_PARTITION_KEY_LENGTH  || checkhash) {
-      candidate = crypto.createHash("sha3-512").update(candidate).digest("hex");
-    }
-  } 
-  
   return candidate;
 };
